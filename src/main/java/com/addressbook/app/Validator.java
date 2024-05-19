d2abec6b03b1261fe9df53a3da57509e4fcdc787
Joshua Sonde (sondej) <joshua_sonde@icloud.com>
2024-05-19 02:09:41 +0100

    FEAT: wrote tests and functionality code for validEmail in validator class

--- src/main/java/com/addressbook/app/Validator.java
+++ src/main/java/com/addressbook/app/Validator.java
@@ -7,9 +7,15 @@ public class Validator {
 
     //will support most Latin-script names
     final Pattern nameRegex = Pattern.compile("^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)");
+    final Pattern phoneNumberRegex = Pattern.compile("^(07\\d{8,12}|447\\d{7,11})$");
 
     public boolean validName(String name){
         Matcher matcher = nameRegex.matcher(name);
+        return matcher.matches();
+    }
+
+    public boolean validPhoneNumber(String number) {
+        Matcher matcher = phoneNumberRegex.matcher(number);
         return matcher.matches();
     }
 }
--- src/test/java/com/addressbook/test/ValidatorTest.java
+++ src/test/java/com/addressbook/test/ValidatorTest.java
@@ -55,4 +55,42 @@ public class ValidatorTest {
             assertFalse(actual);
         }
     }
+
+    @Nested
+    @DisplayName(("Validator Phone Number Tests"))
+    class ValidatorPhoneNumberTests {
+        @Test
+        @DisplayName("validPhoneNumber should return true if input is valid")
+        public void validPhoneNumberTrueWhenInputIsValid() {
+            //Arrange
+            Validator validator = new Validator();
+            //Act
+            boolean actual = validator.validPhoneNumber("07974884593");
+            //Assert
+            assertTrue(actual);
+        }
+
+        @Test
+        @DisplayName("validPhoneNumber should return false if input is not valid")
+        public void validPhoneNumberFalseWhenInputIsInvalid() {
+            //Arrange
+            Validator validator = new Validator();
+            //Act
+            boolean actual = validator.validPhoneNumber("019283947483");
+            //Assert
+            assertFalse(actual);
+        }
+
+        @Test
+        @DisplayName("validPhoneNumber should return false if extreme input is not valid")
+        public void validPhoneNumberFalseWhenExtremeInputIsInvalid() {
+            //Arrange
+            Validator validator = new Validator();
+            //Act
+            boolean actual = validator.validPhoneNumber("dsfjhfdsgkjijgdkjghdhjtsgntrgufhjgdjhghughduhruhgufnhjfudhfurtg ./ dsfjhfdsgkjijgdkjghdhjtsgntrgufrtfgdgdfhssdhjsduhruhgufnhjfudhfurtg");
+            //Assert
+            assertFalse(actual);
+        }
+    }
+
 }
